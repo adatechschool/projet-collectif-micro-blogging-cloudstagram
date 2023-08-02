e5940930b0f83c56da74b7e97a37011bf3ed8bfd
justinelambert12 <justine-lambert@hotmail.fr>
2023-08-02 14:29:39 +0200

    Prepare Post and User models to define relations between them

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -17,4 +18,9 @@ class Post extends Model
     protected $casts = [
         'date' => 'datetime',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }

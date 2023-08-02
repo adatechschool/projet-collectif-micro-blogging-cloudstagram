d15fb904a6acc4f599e7973ef76badbd232ebaee
justinelambert12 <justine-lambert@hotmail.fr>
2023-08-02 17:00:34 +0200

    Create database seeding to seed db with users and multiple posts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,19 +4,18 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run() 
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(5)->create()->each(function ($user) {
+            $user->posts()->saveMany(Post::factory(2)->make());
+        });
     }
 }

82d933e1a3af46470ec75820f8ff7f51269ce5d5
Malena Guallar <malena.guallar@gmail.com>
2023-08-03 15:52:49 +0200

    created form for posting

--- resources/views/postCreation.blade.php
+++ resources/views/postCreation.blade.php
@@ -5,5 +5,29 @@
         </h2>
     </x-slot>
     
-coucou
+    <x-guest-layout>
+        <form method="POST" action="{{ route('postCreation') }}">
+            @csrf
+    
+            <!-- Title -->
+            <div>
+                <x-input-label for="title" :value="__('Title')" />
+                <x-text-input id="title" class="block mt-1 w-full" type="text" name="title" :value="old('title')" required autofocus autocomplete="title" />
+                <x-input-error :messages="$errors->get('title')" class="mt-2" />
+            </div>
+    
+            <!-- Content -->
+            <div class="mt-4">
+                <x-input-label for="content" :value="__('Content')" />
+                <x-textarea id="content" class="block mt-1 w-full" type="text" name="content" :value="old('content')" required autocomplete="content" />
+                <x-input-error :messages="$errors->get('content')" class="mt-2" />
+            </div>
+    
+                <x-primary-button class="ml-4">
+                    {{ __('Post') }}
+                </x-primary-button>
+            </div>
+        </form>
+    </x-guest-layout>
+    
 </x-app-layout>

37d485135e6bdfcc855177c9554d503131b5102b
Malena Guallar <malena.guallar@gmail.com>
2023-08-07 17:14:34 +0200

    styling posts template

--- resources/views/components/post-component.blade.php
+++ resources/views/components/post-component.blade.php
@@ -1,6 +1,10 @@
 @props(['post'])
 
-<div class="post">
-    <h3>{{ $post->title }}</h3>
-    <p>{{ $post->content }}</p>
+<div class="flex flex-row ml-10 p-6">
+    <div class="text-3xl font-bold text-red-300 absolute top-36 left-10 w-4 rotate-90">.ೃ࿐</div>
+    <div class="flex flex-col">    
+        <h3 class=" text-xl font-bold text-red-400 mb-1">{{ $post->title }}</h3>
+        <p class=" mb-3">“ {{ $post->content }} „</p>
+        <p class=" text-sm italic">published by user n° {{ $post->user_id }} on {{ $post->created_at }}</p>
+</div>
 </div>

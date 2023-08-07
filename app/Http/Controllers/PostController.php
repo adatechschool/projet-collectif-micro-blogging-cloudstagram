7063084a792f12e7d190f407d645040acae07686
Malena Guallar <malena.guallar@gmail.com>
2023-08-07 17:14:34 +0200

    styling posts template

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     function displayOne (string $id) {
-        $post = Post::find($id, ['id', 'title', 'content']);
+        $post = Post::find($id, ['id', 'title', 'content', 'user_id', 'created_at']);
     
         if (!$post) {
             abort(404, 'Post not found');

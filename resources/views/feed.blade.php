76a15858a1d2b5daa59a68d824cfc30deb29c665
justinelambert12 <justine-lambert@hotmail.fr>
2023-08-08 10:25:50 +0200

    Integrate post-component in feed to display the posts

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -5,23 +5,8 @@
         </h2>
     </x-slot>
 
-    <table class='border-separate border-8 border-double border-black'>
-        <thead class='border-4 border-black'>
-            <tr>
-            @foreach (array_keys($posts->first()) as $key)
-                <th class='border border-black'>{{ $key }}</th>
-            @endforeach
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($posts as $post)
-                <tr>
-                @foreach ($post as $key=>$value)
-                    <td class='border border-black'>{{ $value }}</td>
-                @endforeach
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+    @foreach ($posts as $post)
+    <x-post-component :post="$post"></x-post-component>
+    @endforeach
 
 </x-app-layout>

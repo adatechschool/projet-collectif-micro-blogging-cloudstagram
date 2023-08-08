efb97b542fca3fe12a94600d23c12272e2a917ad
Malena Guallar <malena.guallar@gmail.com>
2023-08-08 12:30:01 +0200

    styling feed page with hover

--- resources/views/components/post-component.blade.php
+++ resources/views/components/post-component.blade.php
@@ -1,6 +1,6 @@
 @props(['post'])
 
-<div class="flex ml-10 p-6 w-96 opacity-30 hover:opacity-100">
+<div class="flex ml-10 p-6 w-96 opacity-30 hover:opacity-100 transition ease-in-out delay-100">
     <div class="flex flex-col">    
         <h3 class=" text-xl font-bold text-red-400 mb-1">.ೃ࿐ {{ $post['title'] }}</h3>
         <p class=" mb-3">“ {{ $post['content'] }} „</p>

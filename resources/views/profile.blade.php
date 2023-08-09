379567b80a943ef308e0fe51f32b0d6b8080d0ca
Malena Guallar <malena.guallar@gmail.com>
2023-08-09 15:52:11 +0200

    styling profile page

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -8,11 +8,11 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">{{ $user->name }}</div>
-                <div class="p-6 text-gray-900">{{ $user->biography }}</div>
+                <div class="p-6 text-red-400 text-2xl">{{ $user->name }}</div>
+                <div class="p-6 text-gray-900 italic">Bio :  “ {{ $user->biography }} „</div>
                 
                 
-                <div class="p-6 text-gray-900">
+                <div class="p-6 text-red-400 text-xl">
                     {{ __('My Posts') }}
                 </div>
                     <div class="text-xl font-semibold mb-4">
